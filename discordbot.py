f06c218501de222ee992ccb145797f3a5b4e8125
EatEgg <kikikiki4kikiki3@yahoo.co.jp>
2021-04-18 23:54:20 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 import os
 import traceback
 
@@ -14,8 +15,37 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # あっははあ
+    if message.content == "!testjoin":
+        if message.author.voice is None:
+            await message.channel.send("あなたはボイスチャンネルに接続していません。")
+            return
 
+        await message.author.voice.channel.connect()
+        await message.channel.send("接続しました。")
+    # leaveする
+    elif message.content == "!testleave":
+        if message.guild.voice_client is None:
+            await message.channel.send("接続していません。")
+            return
+
+        # 切断する
+        await message.guild.voice_client.disconnect()
+
+        await message.channel.send("切断しました。")
+
+    if message.content == "!testplay":
+        if message.guild.voice_client is None:
+            await message.channel.send("接続していません。")
+            return
+        message.guild.voice_client.play(discord.FFmpegPCMAudio("test.mp3"))
+
+    # 「/test」と発言したら「にゃーん」が返る処理
+    if message.content == '/test':
+        await message.channel.send('にゃーん')
 
 bot.run(token)
