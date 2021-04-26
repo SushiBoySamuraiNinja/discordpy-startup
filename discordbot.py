c8ea389c8b81a4f791b6d72882976ba8b799108e
EatEgg <kikikiki4kikiki3@yahoo.co.jp>
2021-04-26 20:45:10 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -1,20 +1,17 @@
-from discord.ext import commands
 import discord
 import os
-import traceback
 
-bot = commands.Bot(command_prefix='/')
+
 token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('testがログインしました')
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
+@client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
@@ -48,4 +45,5 @@ async def on_message(message):
     if message.content == '/test':
         await message.channel.send('にゃーん')
 
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
